3aeb4f843119b2e8c85e392fc2fc49a996cddaac
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-04-20 17:58:05 +0300

    hw-js8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,19 +6,28 @@ const KEY = 'feedback-form-state';
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(saveData, 500));
 loadData();
-function saveData() {
-  const dataInput = {
+
+function getData() {
+  return {
     email: emailValue.value,
     message: messageValue.value,
   };
+}
+function saveData() {
+  const dataInput = getData();
   const dataInJSON = JSON.stringify(dataInput);
   localStorage.setItem(KEY, dataInJSON);
 }
 
 function onFormSubmit(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
+  const dataInput = getData();
+
+  if (dataInput.email === '' || dataInput.message === '') {
+    alert('Fill in all the fields');
+    event.preventDefault();
+  }
   localStorage.removeItem(KEY);
+  console.log(dataInput);
 }
 
 function loadData() {
